31a05e6c0826f06494edf2d5967e6a3045f06fb4
TitoSaposnik <tito.saposnik@gmail.com>
2022-03-08 01:29:17 -0300

    Primer commit

---
+++ js/01.js
@@ -0,0 +1,20 @@
+//Variables con var (no usado en la actualidad)
+
+var producto = "Audifonos Gamer"; // iniciar variable y asignarle valor.
+var disponible; // iniciamos la variable pero sin valor.
+producto = true; //reasignando valor de la variable.
+
+var producto1 = "Mouse",
+    producto2 = "Teclado",
+    producto3 = "Parlantes";
+
+// var 1producto =  3; error no permitido
+// var -producto = true; error no permitido
+var _producto = true;
+
+//Estilos para las variables:
+var producto_principal; // underscore
+var productoPrincipal; // camelcase
+var ProductoPrincipal; // pascal case (se usa para las clases de js)
+
+console.log(producto)
